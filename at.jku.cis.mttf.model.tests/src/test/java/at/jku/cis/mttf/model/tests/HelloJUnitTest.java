0f6f6fad47dc052706c71e20302cd3f52449fb46
timeraider@gmx.at <timeraider@gmx.at>
2016-10-30 19:21:32 +0100

    added first test project

---
+++ at.jku.cis.mttf.model.tests/src/test/java/at/jku/cis/mttf/model/tests/HelloJUnitTest.java
@@ -0,0 +1,14 @@
+package at.jku.cis.mttf.model.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloJUnitTest {
+
+	@Test
+	public void test() {
+		// Assert.fail("JUnit test failed!");
+		Assert.assertTrue(true);
+	}
+	
+}
